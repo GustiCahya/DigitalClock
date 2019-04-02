c63446545c9de8bcc0fb949cfa54ace6e96c187a
Gusti Cahya <gusticahyax@yahoo.com>
2019-04-02 17:18:52 +0800

    finish version 1.0

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
- 
+const hour_span = document.querySelector('#hour');
+const minute_span = document.querySelector('#minute');
+const second_span = document.querySelector('#second');
+
+function clock() {
+    const fullDate = new Date();
+   
+    let hours = fullDate.getHours();
+    let mins = fullDate.getMinutes();
+    let secs = fullDate.getSeconds();
+
+    (hours < 10) ? hours = '0' + hours:
+    (mins < 10) ? mins = '0' + mins:
+    (secs < 10) ? secs = '0' + secs:
+    'not available';
+
+    hour_span.innerHTML = hours + ':';
+    minute_span.innerHTML = mins + ':';
+    second_span.innerHTML = secs;
+}
+
+setInterval(clock,100);
+
